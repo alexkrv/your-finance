ac7f912a44b6043d5c46df991223b070800c00a0
alexkrv <aleksandr.krv@yandex.ru>
2022-01-10 09:14:09 +0300

    add draft theme switcher

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import { Switch, Button, } from 'antd';
+import { Button, } from 'antd';
 import {
 	EyeOutlined,
-	FlagOutlined,
 	PlusOutlined,
 } from '@ant-design/icons';
 import { useTranslation, } from 'react-i18next';
 
 import './App.css';
 
+import HeaderMenu from './features/headerMenu/HeaderMenu'
+
 function App() {
-	const { t, i18n } = useTranslation();
-	const changeLang = () => {
-		const newLanguage = i18n.resolvedLanguage === 'en' ? 'ru' : 'en'
-		
-		i18n.changeLanguage(newLanguage)
-	}
+	const { t, } = useTranslation();
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<Switch />
+			<HeaderMenu/>
+			<div className="App-header">
 				<Button type="text" shape="circle" icon={<PlusOutlined />} size='large' />
 				<Button type="text" shape="circle" icon={<EyeOutlined />} size='large' />
-				<Button type="secondary" icon={<FlagOutlined />} size='large' onClick={changeLang}>
-					{t('i18n.lang')}
-				</Button>
 				{t('description.part1')}
-			</header>
+			</div>
 		</div>
 	);
 }
---
+++ src/features/headerMenu/HeaderMenu.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useTranslation, } from 'react-i18next';
+import {
+	FlagOutlined,
+} from '@ant-design/icons';
+import { Button, Switch, } from 'antd';
+
+import styles from './HeaderMenu.module.css'
+
+const HeaderMenu = () => {
+	const { t, i18n } = useTranslation();
+	const changeLang = () => {
+		const newLanguage = i18n.resolvedLanguage === 'en' ? 'ru' : 'en'
+        
+		i18n.changeLanguage(newLanguage)
+	}
+    
+	return (
+		<header className={styles.container}>
+			<h1 className={styles.caption}>{t('header.title')}</h1>
+			<div>
+				<Switch/>
+				{t('header.lightTheme')}
+			</div>
+			<Button type="text" icon={<FlagOutlined />} size='large' onClick={changeLang}>
+				{t('i18n.lang')}
+			</Button>
+		</header>
+        
+	)
+}
+
+export default HeaderMenu
