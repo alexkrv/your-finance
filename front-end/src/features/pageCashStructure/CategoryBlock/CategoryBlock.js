4302c15149e4efcc8ae7c568185ba25f639d71c1
alexkrv <aleksandr.krv@yandex.ru>
2023-03-01 09:20:40 +0300

    refactor: decompose CategoryBlock component (part 1)

--- front-end/src/features/pageCashStructure/CategoryBlock/CategoryBlock.js
+++ front-end/src/features/pageCashStructure/CategoryBlock/CategoryBlock.js
@@ -5,84 +5,31 @@ import {
 	DEFAULT_ZERO
 } from 'constants/default-values';
 
-import React, { useMemo, useEffect, } from 'react';
+import React, { useEffect, } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Spin, } from 'antd';
-import { useGetConversionRatesQuery, useAddCashCategoryItemMutation } from 'api/';
+import { useGetConversionRatesQuery } from 'api/';
 import { FinancialValue } from 'components/FinancialValue/FinancialValue';
-import ButtonAddItem from 'components/ButtonAddItem/ButtonAddItem';
 import { Card } from 'components/Card/Card';
 
-import FormAddCashCategory from '../FormAddCashCategory/FormAddCashCategory';
 import { saveTotalSumByType, } from '../PageCashStructureSlice';
 
 import styles from './CategoryBlock.module.scss';
 
 import { CategoryItem } from './CategoryItem/CategoryItem';
+import AddCategoryItem from './AddCategoryItem/AddCategoryItem';
 
 const CategoryBlock = ({ title, type, items, }) => {
 	const dispatch = useDispatch();
 	const { t } = useTranslation();
 	const { baseCurrencyKey } = useSelector(state => state.currencies);
 	const { data, error, isFetching, } = useGetConversionRatesQuery(baseCurrencyKey);
-	const [addCashCategoryItem] = useAddCashCategoryItemMutation();
 	const total = isFetching || error ?
 		DEFAULT_ZERO
 		: parseFloat(items.reduce((acc, el) => acc + el.sourceValue/(data.rates[el.currency].value || 1), DEFAULT_ZERO)
 			.toFixed(1));
-	const memoizedStepInfo = useMemo(() => {
-		const stepsMetaInfo = {
-			[CATEGORY_TYPE_INCOME]: {
-				type: CATEGORY_TYPE_INCOME,
-				title: t('cashCategories.addIncome'),
-				addItemHandler: addCashCategoryItem,
-				sourceInput: {
-					placeholder: t('cashCategories.incomeSourceName'),
-					error: t('cashCategories.errorSourceRequired')
-				},
-				sourceValue: {
-					placeholder: t('cashCategories.incomeSourceValue'),
-					error: t('cashCategories.errorSourceValueRequired'),
-					minValue: DEFAULT_ZERO,
-					maxValue: Number.POSITIVE_INFINITY
-				},
-			},
-			[CATEGORY_TYPE_SPENDING]: {
-				type: CATEGORY_TYPE_SPENDING,
-				title: t('cashCategories.addSpending'),
-				addItemHandler: addCashCategoryItem,
-				sourceInput: {
-					placeholder: t('cashCategories.spendingSourceName'),
-					error: t('cashCategories.errorSourceRequired')
-				},
-				sourceValue: {
-					placeholder: t('cashCategories.spendingSourceValue'),
-					error: t('cashCategories.errorSourceValueRequired'),
-					minValue: DEFAULT_ZERO,
-					maxValue: Number.POSITIVE_INFINITY
-				},
-			},
-			[CATEGORY_TYPE_FROZEN]: {
-				type: CATEGORY_TYPE_FROZEN,
-				title: t('cashCategories.addFrozen'),
-				addItemHandler: addCashCategoryItem,
-				sourceInput: {
-					placeholder: t('cashCategories.frozenSourceName'),
-					error: t('cashCategories.errorSourceRequired')
-				},
-				sourceValue: {
-					placeholder: t('cashCategories.frozenSourceValue'),
-					error: t('cashCategories.errorSourceValueRequired'),
-					minValue: DEFAULT_ZERO,
-					maxValue: Number.POSITIVE_INFINITY
-				},
-			}
-		};
-
-		return [stepsMetaInfo[type]];
-	}, [type, t, addCashCategoryItem]);
 
 	useEffect(() => {
 		if(data && !error) {
@@ -106,12 +53,7 @@ const CategoryBlock = ({ title, type, items, }) => {
 				items.map(item => <CategoryItem item={item} key={item._id}/>)
 				: <div className={styles.noItems}>{t('cashCategories.noItems')}</div>
 			}
-			<ButtonAddItem
-				className={styles.addButton}
-				text={t('cashCategories.addItem')}
-				size='medium'
-				addItemFormElement={<FormAddCashCategory stepsMetaInfo={memoizedStepInfo}/>}
-			/>
+			<AddCategoryItem type={type}/>
 		</Card>
 	);
 };
